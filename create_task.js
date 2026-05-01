bb49cbc2a2dba7dc2660c273f29c2cbffe30bee7
rizki.judojono <rizki.judojono@aroflo.com>
2026-05-01 11:21:12 +1000

    Enable logging for development

--- create_task.js
+++ create_task.js
@@ -2,6 +2,10 @@ import application from "application";
 import { CreateTaskInput } from "./create-task-input.js";
 
 const createTask = async (state) => {
+	if (!application.settings.isProduction()) {
+		console.log("--> Creating task with state:", state);
+	}
+
 	const { prTitle, prAuthor, prFilesChanged } = state;
 
 	const input = new CreateTaskInput({
